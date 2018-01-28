b83d45beee4bf394633b787745395ef8505754c4
qmsalas <qmsalas@iastate.edu>
2018-01-28 14:06:23 -0600

    add login activity

--- SnapClone/openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ SnapClone/openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/QuinnSalas/Documents/Code/AndroidStudio/SnapClone/openCVLibrary340/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/QuinnSalas/Documents/Code/VC4/SnapClone/openCVLibrary340/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
