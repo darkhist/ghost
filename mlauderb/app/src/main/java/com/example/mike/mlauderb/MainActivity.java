e4a85cea8e1e2e0135b7357d7476c1bcbb9adbcb
Mike <mllauderback@gmail.com>
2018-02-14 17:25:17 -0600

    button in main leads to basic friends screen container

--- mlauderb/app/src/main/java/com/example/mike/mlauderb/MainActivity.java
+++ mlauderb/app/src/main/java/com/example/mike/mlauderb/MainActivity.java
@@ -12,16 +12,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set to home screen
         setContentView(R.layout.activity_main);
+        // set to friends screen
+//        setContentView(R.layout.activity_friends);
 
-        // Camera button
-        Button cameraInit = findViewById(R.id.cameraInit);
-        cameraInit.setOnClickListener(new View.OnClickListener() {
+//        // Camera button
+//        Button cameraInit = findViewById(R.id.cameraInit);
+//        cameraInit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getApplicationContext(),OpenCVCamera.class);
+//                startActivity(i);
+//            }
+//        });
+        // Friends button (temp)
+        Button friendsInit = findViewById(R.id.friendsInit);
+        friendsInit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),OpenCVCamera.class);
+                Intent i = new Intent(getApplicationContext(),FriendsActivity.class);
                 startActivity(i);
             }
         });
+
     }
 }
