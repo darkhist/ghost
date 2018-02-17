749cdbee807711ec1d096f8d9d6c0fde44990a33
Mike <mllauderback@gmail.com>
2018-02-17 13:35:22 -0600

    dynamically add buttons now

--- mlauderb/app/src/main/java/com/example/mike/mlauderb/FriendsActivity.java
+++ mlauderb/app/src/main/java/com/example/mike/mlauderb/FriendsActivity.java
@@ -1,11 +1,13 @@
 package com.example.mike.mlauderb;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.*;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -35,6 +37,30 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        // create overall master container
+        RelativeLayout parentContainer = (RelativeLayout) findViewById(R.id.rl);
+
+        // create scrollable container for friends and set width/height to parents width/height
+        ScrollView scrollContainer = new ScrollView(this);
+        scrollContainer.setLayoutParams(new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT, ScrollView.LayoutParams.MATCH_PARENT));
+
+        // create linear vertical container for buttons and set width/height to scrollContainer's width/height
+        LinearLayout buttonContainer = new LinearLayout(this);
+        buttonContainer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        buttonContainer.setOrientation(LinearLayout.VERTICAL);
+        scrollContainer.addView(buttonContainer); // add buttonContainer to scrollContainer
+
+        // add "i" buttons and label them accordingly
+        for (int i = 0; i < 3; i++) {
+            Button btn = new Button(this);
+            btn.setWidth(buttonContainer.getWidth());
+            btn.setText("Sample Friend " + i);
+            buttonContainer.addView(btn);
+        }
+
+        // add scrollContainer to master container
+        parentContainer.addView(scrollContainer);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
