4911123c828d8abf3a97bfb34ce9da64a2783a77
Mike <mllauderback@gmail.com>
2018-02-18 20:38:55 -0600

    changing Volley code accessibility

--- client/app/src/main/java/floatingheads/snapclone/AddFriendsActivity.java
+++ client/app/src/main/java/floatingheads/snapclone/AddFriendsActivity.java
@@ -34,35 +34,16 @@ public class AddFriendsActivity extends AppCompatActivity {
 
         // Button to call OpenCV Camera Activity
         Button button = (Button) findViewById(R.id.button);
+        TextView mTextView = (TextView) findViewById(R.id.textView);
+
+        VolleyActions va = new VolleyActions(this);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StringRequest();
+                va.makeStringRequest("https://www.google.com");
+                mTextView.setText(va.getStringRequest());
             }
         });
-    }
-    
-    private void StringRequest() {
-        TextView mTextView = (TextView) findViewById(R.id.textView);
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(context);
-        String url = "https://www.google.com/";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                // Display the first 500 characters of the response string.
-                String tmp = "Response is: " + response.substring(0, 500);
-                mTextView.setText(tmp);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
     }
 }
