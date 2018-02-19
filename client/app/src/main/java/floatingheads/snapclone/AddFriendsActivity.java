adc8d8c3fe569d9986f3d8b705ff0758ab74c165
Mike <mllauderback@gmail.com>
2018-02-18 18:23:42 -0600

    got basic string request working

--- client/app/src/main/java/floatingheads/snapclone/AddFriendsActivity.java
+++ client/app/src/main/java/floatingheads/snapclone/AddFriendsActivity.java
@@ -1,5 +1,6 @@
 package floatingheads.snapclone;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class AddFriendsActivity extends AppCompatActivity {
+
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +37,32 @@ public class AddFriendsActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),StringRequestActivity.class);
-                startActivity(i);
+                StringRequest();
             }
         });
+    }
+    
+    private void StringRequest() {
+        TextView mTextView = (TextView) findViewById(R.id.textView);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = "https://www.google.com/";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                String tmp = "Response is: " + response.substring(0, 500);
+                mTextView.setText(tmp);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- client/app/src/main/java/floatingheads/snapclone/StringRequestActivity.java
+++ client/app/src/main/java/floatingheads/snapclone/StringRequestActivity.java
@@ -11,6 +11,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+
 /**
  * Created by Akira on 2/18/2018.
  */
@@ -23,16 +25,17 @@ public class StringRequestActivity extends AppCompatActivity {
         TextView mTextView = (TextView) findViewById(R.id.textView);
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://www.google.com";
+        String url = "https://www.google.com/";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: " + response.substring(0, 500));
-                    }
-                }, new Response.ErrorListener() {
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                String tmp = "Response is: " + response.substring(0, 500);
+                mTextView.setText(tmp);
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 mTextView.setText("That didn't work!");
