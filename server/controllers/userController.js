0209155d80ef433f767fa6d45ce1f7a2a1745415
qmsalas <qmsalas@iastate.edu>
2018-02-18 19:36:19 -0600

    add comments

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,17 +12,23 @@ let json = `{
             "creationDate": "2018-02-05"
             }`;
 
+// Handle GET /users
+exports.get = (req, res) => {
+  res.send(json);
+}
+
 // Handle GET /users/create
 exports.create = (req, res) => {
+  // Insert a user into the database
   // TODO
+}
+
+// Handle GET /users/search 
+exports.search = (req, res) => {
+  // Search for a specific user in the database
 }
 
 // Handle GET /users/login
 exports.login = (req, res) => {
   // TODO
-}
-
-// Handle GET /users
-exports.get = (req, res) => {
-  res.send(json);
 }
