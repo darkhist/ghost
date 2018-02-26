bf1dab0a2c78eff0a04960eb424f5b4d8f024ad2
qmsalas <qmsalas@iastate.edu>
2018-02-26 11:57:49 -0600

    update OpenCV path

--- client/app/src/main/jni/Android.mk
+++ client/app/src/main/jni/Android.mk
@@ -6,7 +6,7 @@ OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 
-include /home/mike/Downloads/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+include /Users/QuinnSalas/Downloads/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
