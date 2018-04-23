689e7d0c260e264eb14d2395514a95670c46417e
mlauderb <mlauderb@iastate.edu>
2018-04-23 12:53:29 -0500

    working on profiles now

---
+++ client/app/src/main/java/floatingheads/snapclone/activities/profileViewActivity.java
@@ -0,0 +1,8 @@
+package floatingheads.snapclone.activities;
+
+/**
+ * Created by root on 4/23/18.
+ */
+
+public class profileViewActivity {
+}
--- client/app/src/main/java/floatingheads/snapclone/objects/Profile.java
+++ client/app/src/main/java/floatingheads/snapclone/objects/Profile.java
@@ -1,20 +1,29 @@
 package floatingheads.snapclone.objects;
 
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
 /**
  * Created by Mike on 3/19/18.
  */
 
-public class Profile {
+public class Profile extends ConstraintLayout {
 
-    public Profile(User user) {
-        init(user);
+    User user = null;
+
+    public Profile(Context context, AttributeSet attr) {
+        super(context,attr);
     }
 
-    public void setContents() {
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public void onFinishInflate() {
+        super.onFinishInflate();
 
     }
 
-    public void init(User user) {
-
-    }
 }
