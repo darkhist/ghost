7bbee307df14fb35724476bc5180f84d030b6cda
Akira DeMoss <akirademoss@gmail.com>
2018-04-23 23:11:49 -0500

    pre-merge commit

--- client/app/src/main/java/floatingheads/snapclone/activities/FilterAdapter.java
+++ client/app/src/main/java/floatingheads/snapclone/activities/FilterAdapter.java
@@ -1,96 +1,55 @@
 package floatingheads.snapclone.activities;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 /**
  * Created by why8222 on 2016/3/17.
+ **/
 
-public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.FilterHolder>{
+public class FilterAdapter extends BaseAdapter{
 
-    private MagicFilterType[] filters;
-    private Context context;
-    private int selected = 0;
-
-    public FilterAdapter(Context context, MagicFilterType[] filters) {
-        this.filters = filters;
-        this.context = context;
+    //List<Map<String, Tracker<Face>>> trackerGraphicsList = new ArrayList<Map<String, Tracker<Face>>>();
+    //List<Tracker<Face>> trackerGraphics =
+    @Override
+    public int getCount() {
+        return 0;
     }
 
     @Override
-    public FilterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.filter_item_layout,
-                parent, false);
-        FilterHolder viewHolder = new FilterHolder(view);
-        viewHolder.thumbImage = (ImageView) view.findViewById(R.id.filter_thumb_image);
-        viewHolder.filterName = (TextView) view.findViewById(R.id.filter_thumb_name);
-        viewHolder.filterRoot = (FrameLayout)view.findViewById(R.id.filter_root);
-        viewHolder.thumbSelected = (FrameLayout) view.findViewById(R.id.filter_thumb_selected);
-        viewHolder.thumbSelected_bg = view.findViewById(R.id.filter_thumb_selected_bg);
-        return viewHolder;
+    public Object getItem(int position) {
+        return null;
     }
 
     @Override
-    public void onBindViewHolder(FilterHolder holder,final int position) {
-        holder.thumbImage.setImageResource(FilterTypeHelper.FilterType2Thumb(filters[position]));
-        holder.filterName.setText(FilterTypeHelper.FilterType2Name(filters[position]));
-        holder.filterName.setBackgroundColor(context.getResources().getColor(FilterTypeHelper.FilterType2Color(filters[position])));
-        if(position == selected){
-            holder.thumbSelected.setVisibility(View.VISIBLE);
-            holder.thumbSelected_bg.setBackgroundColor(context.getResources().getColor(FilterTypeHelper.FilterType2Color(filters[position])));
-            holder.thumbSelected_bg.setAlpha(0.7f);
-        }else {
-            holder.thumbSelected.setVisibility(View.GONE);
-        }
-
-        holder.filterRoot.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if(selected == position)
-                    return;
-                int lastSelected = selected;
-                selected = position;
-                notifyItemChanged(lastSelected);
-                notifyItemChanged(position);
-                onFilterChangeListener.onFilterChanged(filters[position]);
-            }
-        });
+    public long getItemId(int position) {
+        return 0;
     }
 
     @Override
-    public int getItemCount() {
-        return filters == null ? 0 : filters.length;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
     }
 
-    class FilterHolder extends RecyclerView.ViewHolder {
-        ImageView thumbImage;
-        TextView filterName;
-        FrameLayout thumbSelected;
-        FrameLayout filterRoot;
-        View thumbSelected_bg;
-
-        public FilterHolder(View itemView) {
-            super(itemView);
-        }
-    }
-
-    public interface onFilterChangeListener{
-        void onFilterChanged(MagicFilterType filterType);
-    }
-
-    private onFilterChangeListener onFilterChangeListener;
-
-    public void setOnFilterChangeListener(onFilterChangeListener onFilterChangeListener){
-        this.onFilterChangeListener = onFilterChangeListener;
+    @Nullable
+    @Override
+    public CharSequence[] getAutofillOptions() {
+        return new CharSequence[0];
     }
 }
- */
--- client/app/src/main/java/floatingheads/snapclone/activities/ImageViewActivity.java
+++ client/app/src/main/java/floatingheads/snapclone/activities/ImageViewActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
