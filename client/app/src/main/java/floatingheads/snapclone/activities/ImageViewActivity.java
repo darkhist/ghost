5a1a9833b12e92263b786ce01aafd9015d8413a8
qmsalas <qmsalas@iastate.edu>
2018-04-23 22:36:20 -0500

    wip: fixing image rotation issue

--- client/app/src/main/java/floatingheads/snapclone/activities/ImageViewActivity.java
+++ client/app/src/main/java/floatingheads/snapclone/activities/ImageViewActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -102,5 +101,4 @@ public class ImageViewActivity extends AppCompatActivity {
             }
         });
     }
-
 }
